0766b509dd118cfe0bc4ed4d9c8eaafb0648f70d
Lennard Beers <l.beers@outlook.de>
2021-08-13 00:32:17 +0200

    added map locator window

--- src/Assets/_Project/Gulde/Population/MapComponent.cs
+++ src/Assets/_Project/Gulde/Population/MapComponent.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Gulde.Pathfinding;
 using Sirenix.OdinInspector;
 using Sirenix.Serialization;
@@ -9,12 +11,52 @@ namespace Gulde.Population
     public class MapComponent : SerializedMonoBehaviour
     {
         [OdinSerialize]
+        [ListDrawerSettings(Expanded = true)]
+        HashSet<EntityComponent> Entities { get; set; } = new HashSet<EntityComponent>();
+
+        [OdinSerialize]
         [ReadOnly]
         public NavComponent NavComponent { get; private set; }
+
+        public event EventHandler<EntityEventArgs> EntityRegistered;
+        public event EventHandler<EntityEventArgs> EntityUnregistered;
+
+        public bool IsEntityRegistered(EntityComponent entityComponent) => Entities.Contains(entityComponent);
 
         void Awake()
         {
             NavComponent = GetComponent<NavComponent>();
         }
+
+        public void RegisterEntity(EntityComponent entityComponent)
+        {
+            if (!entityComponent) return;
+
+            Entities.Add(entityComponent);
+
+            entityComponent.Map = this;
+
+            EntityRegistered?.Invoke(this, new EntityEventArgs(entityComponent));
+        }
+
+        public void UnregisterEntity(EntityComponent entityComponent)
+        {
+            if (!entityComponent) return;
+
+            Entities.Remove(entityComponent);
+
+            entityComponent.Map = null;
+
+            EntityUnregistered?.Invoke(this, new EntityEventArgs(entityComponent));
+        }
+
+        #region OdinInspector
+
+        void OnValidate()
+        {
+            NavComponent = GetComponent<NavComponent>();
+        }
+
+        #endregion
     }
 }
