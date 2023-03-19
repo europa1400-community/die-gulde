6fd7a0ec26a6a8d78f86af7665acaf42a05fbdb9
Nikolaos Ginos <nikolaos.gkinos@googlemail.com>
2023-03-19 19:19:56 +0100

    feat: implement basic graphics decoder
    * Modified graphics decoder to successfully decode the first three graphics.
    Output includes a 4 pixel border on the left. Decoder does not work for the rest of the graphics.
    
    * Decoder now generate a lot more files successfully and removes black bars
    
    * Graphics decoder now takes input and output arguments similar to decoder.py
    
    * feat: decode additional values in graphic header
    
    ---------
    
    Co-authored-by: Lennard Beers <l.beers@outlook.de>

--- decoder/graphics_decoder.py
+++ decoder/graphics_decoder.py
@@ -1,5 +1,38 @@
 import os
+import tkinter as tk
+from tkinter import filedialog
+import argparse
 from PIL import Image
+
+
+def main():
+    parser = argparse.ArgumentParser(description='gilde-decoder')
+    parser.add_argument('-i', '--input', help='input path')
+    parser.add_argument('-o', '--output', help='output path')
+
+    args = parser.parse_args()
+
+    if not args.input:
+        root = tk.Tk()
+        root.withdraw()
+        args.input = filedialog.askdirectory()
+        root.destroy()
+        args.input = args.input.replace('/', '\\')
+
+    if not args.output:
+        args.output = os.path.join(os.getcwd(), 'output')
+
+    if not os.path.exists(args.input):
+        print('input path does not exist')
+        return
+
+    if not os.path.exists(args.output):
+        os.mkdir(args.output)
+
+    gfx_path = os.path.join(args.input, "gfx")
+    gfx_file_path = os.path.join(gfx_path, "Gilde_add_on_german.gfx")
+
+    decode_graphics(gfx_file_path, args.output)
 
 
 def decode_graphics(input_path: str, output_path: str):
@@ -8,7 +41,7 @@ def decode_graphics(input_path: str, output_path: str):
 
     if not os.path.exists(input_path):
         raise FileNotFoundError("Input file not found")
-    
+
     output_graphics_path = os.path.join(output_path, "graphics")
 
     if not os.path.exists(output_graphics_path):
@@ -19,37 +52,85 @@ def decode_graphics(input_path: str, output_path: str):
 
         graphics_count = int.from_bytes(header_data, byteorder="little", signed=False)
 
-        print(f"Found {graphics_count} graphics")
-
-        graphics = []
+        graphics_headers = []
 
         for i in range(graphics_count):
             graphics_header_data = file.read(84)
-            graphics_name = graphics_header_data[:32].decode("ascii").strip("\x00")
-            graphics_relative_start_address = int.from_bytes(graphics_header_data[48:52], byteorder="little", signed=False)
+            graphics_name = graphics_header_data[:48].decode("ascii").strip("\x00")
+            graphics_start_address = int.from_bytes(graphics_header_data[48:52], byteorder="little", signed=False)
+            shapbank_size = int.from_bytes(graphics_header_data[56:60], byteorder="little", signed=False)
             graphics_width = int.from_bytes(graphics_header_data[80:82], byteorder="little", signed=False)
             graphics_height = int.from_bytes(graphics_header_data[82:84], byteorder="little", signed=False)
 
-            print(f"Decoding {graphics_name} ({graphics_width}x{graphics_height}) at +{graphics_relative_start_address}")
-
-            graphics.append({
+            graphics_headers.append({
                 "name": graphics_name,
-                "relative_start_address": graphics_relative_start_address,
+                "start_address": graphics_start_address,
+                "size": shapbank_size,
                 "width": graphics_width,
                 "height": graphics_height
             })
 
-        for graphic in graphics:
-            file.seek(graphic["relative_start_address"], os.SEEK_CUR)
+        for graphic_header in graphics_headers:
+            file.seek(graphic_header["start_address"])
 
-            graphic_data = file.read(graphic["width"] * graphic["height"] * 3)
+            shapbank_data = file.read(graphic_header["size"])
 
-            img = Image.frombytes(mode="RGB", size=(graphic["width"], graphic["height"]), data=graphic_data)
+            graphics_count = int.from_bytes(shapbank_data[42:44], byteorder="little", signed=False)
 
-            img.show()
+            # What is this information used for?
+            max_width = int.from_bytes(shapbank_data[44:46], byteorder="little", signed=False)
+            max_width = int.from_bytes(shapbank_data[46:48], byteorder="little", signed=False)
 
+            # Shapbank size is stored twice? In the file header and in the Shapbank header
+            shapbank_size = int.from_bytes(shapbank_data[48:52], byteorder="little", signed=False)
 
+            graphics_size_offset = 69
+            graphics_offsets = []
+            for i in range(graphics_count):
+                offset = graphics_size_offset + 4 * i
+                graphics_offsets.append(
+                    int.from_bytes(shapbank_data[offset:offset + 4], byteorder="little", signed=False))
+
+            x = 0
+            for i in graphics_offsets:
+                graphic_data = bytearray()
+
+                file.seek(graphic_header["start_address"] + i)
+                graphic_size = int.from_bytes(file.read(4), byteorder="little", signed=False)
+                num1 = file.read(2)
+                graphic_width = int.from_bytes(file.read(2), byteorder="little", signed=False)
+                num2 = file.read(2)
+                graphic_height = int.from_bytes(file.read(2), byteorder="little", signed=False)
+                num3 = file.read(2) # Always 0x0002
+                num4 = file.read(2)
+                num5 = file.read(2)
+                graphic_width_2 = file.read(2)
+                graphic_height_2 = file.read(2)
+                num6 = file.read(2)
+                num7 = file.read(2)
+                num8 = file.read(2)
+                num9 = file.read(2)
+                file.seek(8, os.SEEK_CUR)
+                num10 = file.read(4)
+                graphic_size_without_footer = file.read(4) # Might help to calculate the actual graphic size
+                num12 = file.read(4) # Seems to be related to the graphic dimensions
+                
+                # This method only works for a subset of graphics
+                border_pixel_count = 4
+                for j in range(graphic_height):
+                    file.read(4 * 3)
+                    graphic_data += bytearray(file.read(graphic_width * 3))
+
+                img = Image.frombytes(mode="RGB", size=(graphic_width, graphic_height), data=bytes(graphic_data))
+
+                if x == 0:
+                    file_path = os.path.join(output_graphics_path, graphic_header["name"] + ".bmp")
+                else:
+                    file_path = os.path.join(output_graphics_path, graphic_header["name"] + "+" + str(x) + ".bmp")
+
+                img.save(file_path)
+                x += 1
 
 
 if __name__ == "__main__":
-    decode_graphics("C:\\Users\\lbeer\\Games\\Die Gilde Gold-Edition\\gfx\\Gilde_add_on_german.gfx", "output")
+    main()
